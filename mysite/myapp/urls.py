e8a52c19eefbc020f44c0e60a321e7ff6cab9866
akshitbhalla2 <akshitbhalla2@gmail.com>
2021-12-26 18:28:55 +0530

    excluded django key

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home_page, name = "myapp-home"),            # View to display at localhost:8000
+    path("about.html", views.about_page, name = "myapp-about"),   # View to display at localhost:8000/about.html
     path("blog.html", views.blog_page, name = "myapp-blog"),   # View to display at localhost:8000/blog.html
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "Akshit Bhalla",
+        "title": "Sports",
+        "content": "A description of an NBA game",
+        "date": "20/12/2021"
+    },
+    {
+        "author": "Tanya Bhalla",
+        "title": "Beauty",
+        "content": "A tutorial on cosmetic items",
+        "date": "12/03/2021"
+    },
+]
+
 def home_page(request):
-    return HttpResponse("<h1>Welcome to this site</h1> <a href='blog.html'>BLOG</a>")
+    return HttpResponse("<h1>Welcome to this site</h1> <a href='about.html'>ABOUT</a> <br> <a href='blog.html'>BLOG</a>")
 
 def blog_page(request):
-    return render(request, "myapp/blog.html")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "myapp/blog.html", context)
+
+def about_page(request):
+    context = {
+        "title": "About Page",
+    }
+    return render(request, "myapp/about.html", context)
