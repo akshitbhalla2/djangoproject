55c755e63cf2f468e9eb97bdbf277ac8da808f2a
akshitbhalla2 <akshitbhalla2@gmail.com>
2021-12-28 23:44:19 +0530

    manually populate database and register myapp

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        "author": "Akshit Bhalla",
-        "title": "Sports",
-        "content": "A description of an NBA game",
-        "date": "20/12/2021"
-    },
-    {
-        "author": "Tanya Bhalla",
-        "title": "Beauty",
-        "content": "A tutorial on cosmetic items",
-        "date": "12/03/2021"
-    },
-]
+from .models import Post
 
 def home_page(request):
     return HttpResponse("<h1>Welcome to this site</h1> <a href='about.html'>ABOUT</a> <br> <a href='blog.html'>BLOG</a>")
 
 def blog_page(request):
     context = {
-        "posts": posts,
+        "posts": Post.objects.all(),
     }
     return render(request, "myapp/blog.html", context)
 
