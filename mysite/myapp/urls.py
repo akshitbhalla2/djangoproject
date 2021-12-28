37ae245a826837561304ad4902d0d2b10424c24b
akshitbhalla2 <akshitbhalla2@gmail.com>
2021-12-28 20:03:42 +0530

    created super user

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home_page, name = "myapp-home"),            # View to display at localhost:8000
-    path("about.html", views.about_page, name = "myapp-about"),   # View to display at localhost:8000/about.html
-    path("blog.html", views.blog_page, name = "myapp-blog"),   # View to display at localhost:8000/blog.html
+    path("", views.home_page, name = "myapp-home"),                # View to display at localhost:8000
+    path("about.html", views.about_page, name = "myapp-about"),    # View to display at localhost:8000/about.html
+    path("blog.html", views.blog_page, name = "myapp-blog"),       # View to display at localhost:8000/blog.html
 ]
